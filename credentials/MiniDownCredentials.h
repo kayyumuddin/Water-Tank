a389e1adff10a04cd4b4a19e714d082e06c008bc
Kayyum Uddin <48589486+kayyumuddin@users.noreply.github.com>
2021-06-04 14:30:52 +0530

    Add files via upload

---
+++ credentials/MiniDownCredentials.h
@@ -0,0 +1,16 @@
+#define AUTH "8WGHp0mOW5nvLK6-VY32ZozsgbxaQgVG"
+#define SSID "Zinat"
+#define PASS "Zinat@#1663"
+
+#define APP_KEY "403b8578-db2b-43cc-9d5a-bef47db33915"
+#define APP_SECRET "c964ffd9-ebc8-4632-934b-939e0292453e-aa4802f4-c6b7-4064-88b8-d781a83386fb"
+#define SWITCH_ID "5fc38bf7dbb2021cadb4c90f"
+
+IPAddress SERVER_ADDRESS (188, 166, 206, 43);
+#define HOSTNAME "Esp8266 Down"
+
+#define RELAY_PIN D5
+
+#define DIVIDE_RATIO 1.98
+#define MULTIPLY_RATIO 56.7
+#define MAX_HEIGHT 214
