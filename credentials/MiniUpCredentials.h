129b3e63d8e9b7628c80851e8bd788ddfa81b260
Kayyum Uddin <48589486+kayyumuddin@users.noreply.github.com>
2021-06-04 14:50:54 +0530

    Update MiniUpCredentials.h

--- credentials/MiniUpCredentials.h
+++ credentials/MiniUpCredentials.h
@@ -1,16 +1,21 @@
-#define AUTH "OpAgY391LMmMQkiqaR0S0r_CblBmWH8C"
-#define SSID "Zinat"
-#define PASS "Zinat@#1663"
+#define AUTH "Blynk_Token"
+#define SSID "WiFi_Ssid"
+#define PASS "WiFi_Pass"
 
-#define APP_KEY "403b8578-db2b-43cc-9d5a-bef47db33915"
-#define APP_SECRET "c964ffd9-ebc8-4632-934b-939e0292453e-aa4802f4-c6b7-4064-88b8-d781a83386fb"
-#define SWITCH_ID "5fb6a99532b3021cb51cf52f"
+#define APP_KEY "SinricPro_App_key"
+#define APP_SECRET "SinricPro_App_Secret"
+#define SWITCH_ID "SinricPro_Switch_Id"
 
 IPAddress SERVER_ADDRESS (188, 166, 206, 43);
 #define HOSTNAME "Esp8266 Up"
 
 #define RELAY_PIN D1
 
-#define DIVIDE_RATIO 1.21
-#define MULTIPLY_RATIO 20
-#define MAX_HEIGHT 132
+#define DIVIDE_RATIO 1.21    // (Depth of Tank that store water in cm) / 100
+//My tank -> 121 cm
+
+#define MULTIPLY_RATIO 20.0  // (Total tank volume in litre) / 100
+//My tank -> 2000 litre
+
+#define MAX_HEIGHT 132       // (Depth of Tank that store water in cm) + (Mounting height of sensor)
+//My tank -> 121cm + 11cm = 132 cm
